ebeb7dcae30d03db352fe60d29637dad788190aa
zbl14 <ifthereisoneday@gmail.com>
2022-07-02 17:50:50 -0700

    add WorthBuying method allowing to display cars which price is lower than MaxPrice

---
+++ Car.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+
+public class Car
+{
+  public string MakeModel;
+  public int Price;
+  public int Miles;
+
+  public bool WorthBuying(int maxPrice)
+  {
+    return (Price <= maxPrice);
+  }  
+}
+
+public class Program
+{
+  public static void Main()
+  {
+    Car volkswagen = new Car();
+    volkswagen.MakeModel = "1974 Volkswagen Thing";
+    volkswagen.Price = 1100;
+    volkswagen.Miles = 368792;
+
+    Car yugo = new Car();
+    yugo.MakeModel = "1980 Yugo Koral";
+    yugo.Price = 700;
+    yugo.Miles = 56000;
+
+    Car ford = new Car();
+    ford.MakeModel = "1988 Ford Country Squire";
+    ford.Price = 1400;
+    ford.Miles = 239001;
+
+    Car amc = new Car();
+    amc.MakeModel = "1976 AMC Pacer";
+    amc.Price = 400;
+    amc.Miles = 198000;
+
+    List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc };
+
+    Console.WriteLine("Enter maximum price: ");
+    string stringMaxPrice = Console.ReadLine();
+    int maxPrice = int.Parse(stringMaxPrice);
+
+    List<Car> CarsMatchingSearch = new List<Car>(0);
+
+    foreach (Car automobile in Cars)
+    {
+      if (automobile.WorthBuying(maxPrice))
+      {
+        CarsMatchingSearch.Add(automobile);
+      }
+    }
+
+    foreach(Car automobile in CarsMatchingSearch)
+    {
+      Console.WriteLine(automobile.MakeModel);
+    }
+  }
+}
