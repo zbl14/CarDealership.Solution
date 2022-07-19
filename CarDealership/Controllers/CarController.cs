919b6b9479816fa738b7e7844f632243bb4350f2
zbl14 <ifthereisoneday@gmail.com>
2022-07-19 08:10:09 -0700

    reoganize the file structure for web based app

---
+++ CarDealership/Controllers/CarController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Dealership.Models;
+using System.Collections.Generic;
+
+namespace Dealership.Controllers
+{
+  public class CarController : Controller
+  {
+
+  }
+}
--- CarDealership/Models/Car.cs
+++ CarDealership/Models/Car.cs
@@ -2,16 +2,23 @@ namespace Dearlership.Models
 {
   public class Car
   {
-    // Public auto-implemented properties
     public string MakeModel { get; set; }
     public int Price { get; set; }
     public int Miles { get; set; }
+
+    private static List<Car> _instances = new List<Car> {};
 
     public Car(string makeModel, int price, int miles)
     {
       MakeModel = makeModel;
       Price = price;
       Miles = miles;
+      _instances.Add(this);
+    }
+
+    public static List<Car> GetAll()
+    {
+      return _instances;
     }
 
     public bool WorthBuying(int maxPrice)
